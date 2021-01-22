64a87b0857c546668072f381624908aa5e3280c1
frankie0917 <frankie0917@foxmail.com>
2021-01-23 04:54:50 +0800

    add mobx state tree

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,53 +1,74 @@
 import { makeAutoObservable, toJS } from "mobx";
-import { types } from "mobx-state-tree";
+import { Instance, types } from "mobx-state-tree";
 import { createContext, PropsWithChildren, useContext } from "react";
 
-export type Cell = "O" | "X" | "-";
+// export type Cell = "O" | "X" | "-";
 
-export type Board = Cell[][];
+// export type Board = Cell[][];
 
-export class Indicator {
-  constructor() {
-    makeAutoObservable(this);
-  }
+// export class Indicator {
+//   constructor() {
+//     makeAutoObservable(this);
+//   }
 
-  value: number[][] = [];
+//   value: number[][] = [];
 
-  addIndicator(value: number[]) {
+//   addIndicator(value: number[]) {
+//     this.value.push(value);
+//   }
 
-    this.value.push(value);
-  }
+//   setIndicator(index: number, value: number[]) {
+//     this.value[index] = value;
+//   }
 
-  setIndicator(index: number, value: number[]) {
-    this.value[index] = value;
-  }
+//   addIndicatorCell(Index: number) {
+//     this.value[Index].push(1);
+//   }
 
-  addIndicatorCell(Index: number) {
-    this.value[Index].push(1);
-  }
+//   setIndicatorCell(Index: number, cellIndex: number, value: number) {
+//     this.value[Index][cellIndex] = value;
+//   }
+// }
 
-  setIndicatorCell(Index: number, cellIndex: number, value: number) {
-    this.value[Index][cellIndex] = value;
-  }
-}
+const Indicator = types.model('indicator',{
+  value: types.array(types.array(types.number))
+})
 
-export class RootStore {
-  constructor() {
-    makeAutoObservable(this);
-  }
-  current: Board = [];
-  success: Board = [];
-  colIndicators: Indicator = new Indicator();
-  rowIndicators: Indicator = new Indicator();
-}
+const Cell = types.union([types.string()])
 
-export const RootStoreContext = createContext<RootStore>(new RootStore());
+const Board = types
+  .model("board", {
+    value: ,
+  })
+  .actions((self) => ({
+    addRow() {
+      self.value.push([]);
+    },
+  }));
+
+const RootStore = types.model("rootStore", {
+  current: Board,
+});
+
+// export class RootStore {
+//   constructor() {
+//     makeAutoObservable(this);
+//   }
+//   current: Board = [];
+//   success: Board = [];
+//   colIndicators: Indicator = new Indicator();
+//   rowIndicators: Indicator = new Indicator();
+// }
+
+interface IRootStore extends Instance<typeof RootStore> {}
+
+export const RootStoreContext = createContext<IRootStore>({} as IRootStore);
 
 export const RootStoreProvider: React.FC<PropsWithChildren<{}>> = ({
   children,
 }) => {
   return (
-    <RootStoreContext.Provider value={new RootStore()}>
+    <RootStoreContext.Provider value={RootStore.create()}>
       {children}
     </RootStoreContext.Provider>
   );
